d1a8f724ca025eb4e16ecccf0f60eb84405fb8eb
Pavel Hamrik <hey@pavelhamrik.com>
2020-12-15 20:55:36 +0100

    Day 10b finally

--- src/day10b2.js
+++ src/day10b2.js
@@ -1,10 +1,25 @@
 import { data } from "./day10.data";
 
-// const DEPTH_LIMIT = 3; // 8 is borderline
-
 const rawJoltages = data.split("\n").map((joltage) => parseInt(joltage, 10));
-const joltages = rawJoltages.sort((a, b) => a - b);
+const joltages = new Set(rawJoltages.concat([0]))
+const maxJoltage = Array.from(joltages.values()).reduce((acc, joltage) => acc < joltage ? joltage : acc, 0)
 
-const maxJoltage = joltages[joltages.length - 1] + 3;
+const mem = new Map();
 
-console.log(joltages.toString())
+const jump = (joltages, target) => {
+    if (!joltages.has(target)) return 0
+    if (target <= 0) return 1
+
+    let bets = 0;
+    for (let i = 1; i <= 3; i += 1) {    
+        if (!mem.has(target - i)) {
+            mem.set(target - i, jump(joltages, target - i))
+        }
+        bets += mem.get(target - i)
+    }
+
+    return bets
+}
+
+// console.log(joltages.toString())
+console.log(jump(joltages, maxJoltage))
--- src/day15b.js
+++ src/day15b.js
@@ -1,36 +1,18 @@
 const data = `16,11,15,0,1,7`
-// const data = `0,3,6`
-// const data = `0,3,6`
 const starter = data.split(',').map(number => parseInt(number, 10))
 // const STOPPER = 2020
 const STOPPER = 30000000
-
-// const STOPPER = 30 // 0.339 ms
-// const STOPPER = 300 // 0.802 ms
-// const STOPPER = 3000 // 3.666 ms
-// const STOPPER = 30000 // 171.042 ms
-// const STOPPER = 300000 // 14379.704 ms
-// const STOPPER = 3000000 // 14379.704 ms
 
 console.time('Run')
 
 const play = (starter) => {
     const numbers = new Map()
     starter.slice(0, starter.length - 1).forEach((number, index) => numbers.set(number, index + 1))
-    // starter.forEach((number, index) => numbers.set(number, index + 1))
     let curr = starter[starter.length - 1]
-
-    // let lastNumber
-
-    // console.log(curr, numbers)
 
     for (let i = starter.length + 1; i <= STOPPER; i += 1) {
         const next = numbers.has(curr) ? i - 1 - numbers.get(curr) : 0
-        
         numbers.set(curr, i - 1)
-
-        // console.log(`turn ${i}, curr ${curr}, next ${next}`, numbers)
-
         curr = next
     }
     return curr
@@ -38,3 +20,5 @@ const play = (starter) => {
 
 console.log(play(starter))
 console.timeEnd('Run')
+
+// = 37312
