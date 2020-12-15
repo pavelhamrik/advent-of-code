7f824927eb0382d19619e52d9fda0fcd4baf75f8
Pavel Hamrik <hey@pavelhamrik.com>
2020-12-15 13:49:45 +0100

    Day 15b

--- src/day15b.js
+++ src/day15b.js
@@ -1,7 +1,9 @@
-// const data = `16,11,15,0,1,7`
-const data = `0,3,6`
+const data = `16,11,15,0,1,7`
+// const data = `0,3,6`
+// const data = `0,3,6`
 const starter = data.split(',').map(number => parseInt(number, 10))
-const STOPPER = 10
+// const STOPPER = 2020
+const STOPPER = 30000000
 
 // const STOPPER = 30 // 0.339 ms
 // const STOPPER = 300 // 0.802 ms
@@ -15,16 +17,19 @@ console.time('Run')
 const play = (starter) => {
     const numbers = new Map()
     starter.slice(0, starter.length - 1).forEach((number, index) => numbers.set(number, index + 1))
+    // starter.forEach((number, index) => numbers.set(number, index + 1))
     let curr = starter[starter.length - 1]
+
+    // let lastNumber
 
     // console.log(curr, numbers)
 
-    for (let i = starter.length; i <= STOPPER; i += 1) {
+    for (let i = starter.length + 1; i <= STOPPER; i += 1) {
         const next = numbers.has(curr) ? i - 1 - numbers.get(curr) : 0
         
-        numbers.set(curr, i)
+        numbers.set(curr, i - 1)
 
-        console.log(`turn ${i}, curr ${curr}, next ${next}`, numbers)
+        // console.log(`turn ${i}, curr ${curr}, next ${next}`, numbers)
 
         curr = next
     }
